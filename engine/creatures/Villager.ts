970a0f769f032b4bf00c6b5cd8143feb24f81648
Ohkubo KOHEI <kuboon@trick-with.net>
2022-01-14 10:08:45 +0000

    wip

---
+++ engine/creatures/Villager.ts
@@ -0,0 +1,2 @@
+export const name = "Villager";
+export const actions = ["vote"];
--- engine/types.ts
+++ engine/types.ts
@@ -1,4 +1,4 @@
-export type Reguration = {
+export type Rule = {
   vote: "public" | "private";
   timer: number;
 };
@@ -16,35 +16,49 @@ export enum Team {
   Villagers = "villagers",
   Wolves = "wolves",
 }
-export type Creature ={
+export type Creature = {
   id: string;
   name: string;
   type: CreatureType;
   team: Team;
   die_of: string;
   killed_by: string;
-}
-export type Action = {
-  type: "vote" | "kill" | "see" | "guard" | "shinobi";
+};
+export type Vote = {
+  type: "vote";
   actor: string;
   target: string;
-} | {
+}
+export type SingleTargetAction = {
+  type: "bite" | "see" | "guard" | "shinobi";
+  actor: string;
+  target: string;
+};
+type BitchAction = {
   type: "bitch";
   actor: string;
   target: string;
   fake: string;
-}
+};
+export type Action = Vote | SingleTargetAction | BitchAction;
 export type Day = {
   num: number;
   actions: Action[];
-}
+};
+export type Result = {
+  num: number;
+  voted: string,
+  died: string[],
+};
 export type VillageState = {
+  rule: Rule;
   creatues: Creature[];
   days: Day[];
+  results: Result[];
   result: {
     winner: {
       team: Team;
       creature_ids: string[];
-    }
-  }
+    };
+  };
 };
