673f1feffa995972fb366ed4a6acdf35522a8ee7
Ohkubo KOHEI <kuboon@trick-with.net>
2022-01-24 02:34:57 +0000

    simplify actions & logs

--- engine/test.ts
+++ engine/test.ts
@@ -1,29 +1,22 @@
 import { assertEquals } from "https://deno.land/std@0.121.0/testing/asserts.ts";
-import { Noon, Village } from "../types.ts";
+import { Village } from "../types.ts";
 import { nightPhase } from "./village.ts";
 
 const aDay = () => ({
-  noon: {
-    votes: [],
-    actions: [],
-  },
-  night: {
-    voted: null,
-    logs: [],
-    died: [],
-  },
+  actions: [],
+  logs: [],
 });
 function aVillage(days = [aDay()]): Village {
   return {
     rule: {
       vote: "public",
     },
-    creatues: [
-      { id: "v", role: {type: "villager"} },
-      { id: "s", role: {type: "seer"} },
-      { id: "g", role: {type: "bodyguard"} },
-      { id: "w", role: {type: "wolf"} },
-      { id: "l", role: {type: "lover"} }
+    creatures: [
+      { id: "v", role: { type: "villager" } },
+      { id: "s", role: { type: "seer" } },
+      { id: "g", role: { type: "bodyguard" } },
+      { id: "w", role: { type: "wolf" } },
+      { id: "l", role: { type: "lover" } },
     ],
     days,
   };
@@ -31,51 +24,37 @@ function aVillage(days = [aDay()]): Village {
 Deno.test({
   name: "bite kills",
   fn: () => {
-    const v: Village = aVillage()
-    const noon: Noon = {
-      votes: [],
-      actions: [{ type: "bite", actor: "w", target: "s" }],
-    };
-    const next = nightPhase(v, noon);
-    assertEquals(next.lastDay.night.died[0], { id: "s", reason: "bite" });
+    const v: Village = aVillage();
+    const actions = [{ type: "bite", actor: "w", target: "s" }];
+    const next = nightPhase(v, actions);
+    assertEquals(next.creature("s").alive, false);
   },
 });
 Deno.test({
   name: "body guard protects bite",
   fn: () => {
-    const v: Village = aVillage()
-    const noon: Noon = {
-      votes: [],
-      actions: [
-        { type: "guard", actor: "g", target: "s" },
-        { type: "bite", actor: "w", target: "s" }
-      ],
-    };
-    const next = nightPhase(v, noon);
-    assertEquals(next.lastDay.night.died, []);
+    const v: Village = aVillage();
+    const actions = [
+      { type: "guard", actor: "g", target: "s" },
+      { type: "bite", actor: "w", target: "s" },
+    ];
+    const next = nightPhase(v, actions);
+    assertEquals(next.creature("s").alive, true);
   },
 });
 
 Deno.test({
   name: "loved voted and suicide",
   fn: () => {
-    const v: Village = aVillage([])
-    const noon0: Noon = {
-      votes: [],
-      actions: [
-        { type: "propose", actor: "l", target: "v" },
-      ],
-    };
-    const day0 = nightPhase(v, noon0);
-    const noon1: Noon = {
-      votes: [{
-        type: 'vote',
-        actor: 'v',
-        target: 'l'
-      }],
-      actions: []
-    }
-    const day1 = nightPhase(day0.village, noon1);
-    assertEquals(day1.lastDay.night.died, [{ id: 'v', reason: 'suicide' }]);
+    const v: Village = aVillage([]);
+    const actions0 = [
+      { type: "propose", actor: "l", target: "v" },
+    ];
+    const day0 = nightPhase(v, actions0);
+    const actions1 = [
+      { type: "vote", actor: "v", target: "l" },
+    ];
+    const day1 = nightPhase(day0.village, actions1);
+    assertEquals(day1.creature('v').dieOf, "suicide" );
   },
 });
