8a8536a2c273f1617230f85a302d35e2cdabaf7a
Ohkubo KOHEI <o@kbn.one>
2025-02-14 23:40:28 +0900

    minor fix Translator [wip]

--- engine/Translator.ts
+++ engine/Translator.ts
@@ -1,19 +1,22 @@
-import { CreatureId } from "./types";
-import { VillageState } from "./VillageState";
+import { CreatureId } from "../types.ts";
+import { VillageState } from "../VillageState.ts";
 
 export class Translator extends VillageState {
   translateTo(dayNum: number, id?: CreatureId): string[] {
     const lines: string[] = [];
-    for (const l of this.village.days[dayNum].night.logs) {
+    for (const l of this.village.days[dayNum].logs) {
       if (id && l.receivers instanceof Array && !l.receivers.includes(id)) {
         continue;
       }
-      const actor = this.creature(l.actor).name;
-      const target = this.creature(l.target).name;
+      const actor = this.creature(l.actor!).name;
+      const target = this.creature(l.target!).name;
       let line;
       switch (l.action) {
         case "bite":
           line = `${actor}は${target}を襲撃した。`;
+          break;
+        default:
+          line = `${actor}の行動は謎のままだ。`;
       }
       lines.push(line);
     }
